2aeb78de3aba0c2a996a81c42ff335882438872c
fbro1 <64466397+fbro1@users.noreply.github.com>
2020-07-02 15:43:52 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,18 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+#--------------------------------------------------------------
+# This module creates all resources necessary for a VPC
+#--------------------------------------------------------------
+
+variable "name" { default = "vpc" }
+variable "cidr" { }
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "${var.cidr}"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags      { Name = "${var.name}" }
+  lifecycle { create_before_destroy = true }
 }
+
+output "vpc_id"   { value = "${aws_vpc.vpc.id}" }
+output "vpc_cidr" { value = "${aws_vpc.vpc.cidr_block}" }
